838925120d1704f85672685e896ae74be96f9caa
Nicolas Evangelista Machado de Morais <nicolasevangelista.dev@outlook.com>
2026-03-10 22:00:22 -0300

    first commig

---
+++ ImportCA/Class1.cs
@@ -0,0 +1,15 @@
+﻿namespace ImportCA
+{
+    public class FtpImport : IDisposable
+    {
+        private bool _disposed = false;
+
+        public void Dispose()
+        {
+            
+        }
+
+    }
+
+}
+
