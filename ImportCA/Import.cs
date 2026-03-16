88d96d7bed67702d79ed3f8d80c959cbe2e3b4b7
Nicolas Evangelista Machado de Morais <nicolasevangelista.dev@outlook.com>
2026-03-16 09:29:27 -0300

    replace

---
+++ ImportCA/Import.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+using System.Text;
+using System.IO;
+using System.Data.SQLite;
+using FluentFTP;
+
+
+namespace ImportCA
+{
+
+    //Parâmetros de importação.
+    public class SettingsFtpService : ImportFtpService
+    {
+        public void Init()
+        {
+            
+        }
+    }
+
+    //Importação do arquivo ftp.
+    public class ImportFtpService
+    {
+        
+    }
+}
